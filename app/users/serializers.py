422cb8a3c65b82dad147f3272710b3ab38876715
eldelahoz <andelahoz13@gmail.com>
2024-10-01 19:35:42 -0500

    ADD: Se sube el proyecto base como se tenia en el repo del grupo

---
+++ app/users/serializers.py
@@ -0,0 +1,23 @@
+from .models import *
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('__all__')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class ObtainTokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
